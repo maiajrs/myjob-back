5156982642a625cef287be74b1c8d1aa779b2b52
maiajrs <jr.maiafs1@gmail.com>
2023-02-20 14:47:41 -0300

    chore: add cors

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@
 import "reflect-metadata";
 import "express-async-errors";
 import "./shared/container"
+import cors from "cors";
 import express from "express";
 import { CreateClientControllre } from "./modules/clients/useCases/createClient/createClientController";
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 const createClientController = new CreateClientControllre()
